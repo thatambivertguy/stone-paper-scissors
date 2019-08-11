9b0c3e40ae829430ea4e6249444c8b89bfe82e57
Saurabh Gupta <saurabhgupta1699@gmail.com>
2019-08-11 15:35:48 +0530

    added game function
    Signed-off-by: Saurabh Gupta <saurabhgupta1699@gmail.com>

--- app.js
+++ app.js
@@ -16,9 +16,42 @@ $(()=>{
  function game (user_choice){
      const choices=['r','p','s']
      console.log(user_choice) 
-    //  console.log(Math.ceil((Math.random())*3))
-    const comp_choice=choices[Math.ceil((Math.random())*3)]
+    //  console.log(Math.floor((Math.random())*3))
+    const comp_choice=choices[Math.floor((Math.random())*3)]
     console.log(comp_choice)
+    if(user_choice=='r'){
+        if(comp_choice=='r'){
+            console.log('draw')
+        }
+        else if (comp_choice=='p'){
+            console.log("computer wins")
+        }
+        else {
+            console.log("user wins")
+        }
+    }
+    else if(user_choice=='p'){
+        if(comp_choice=='p'){
+            console.log('draw')
+        }
+        else if (comp_choice=='s'){
+            console.log("computer wins")
+        }
+        else {
+            console.log("user wins")
+        }
+    }
 
+    else if(user_choice=='s'){
+        if(comp_choice=='s'){
+            console.log('draw')
+        }
+        else if (comp_choice=='r'){
+            console.log("computer wins")
+        }
+        else {
+            console.log("user wins")
+        }
+    }    
     }
 })
